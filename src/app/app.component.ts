127aab3e005bcde99791ade08dd16247646504c0
alighalebx <alighaleb2001@gmail.com>
2024-05-06 23:53:26 +0300

    navbar-some CSS updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { environment } from './../environments/environment';
+import { Router } from '@angular/router';
+import { AuthService } from './shared/auth.service';
 
 
 @Component({
@@ -8,8 +10,22 @@ import { environment } from './../environments/environment';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  constructor() {
+  constructor(private router: Router, private auth: AuthService) { }
 
+  navigateToCreateRecipe() {
+    // Navigate to the recipe creation page, passing the user ID as a query parameter
+    this.router.navigate(['recipes/create'], { queryParams: { authorId: this.auth.userId } });
+  }
+  navigateToListRecipe() {
+    // Navigate to the recipe creation page, passing the user ID as a query parameter
+    this.router.navigate(['recipes'], { queryParams: { authorId: this.auth.userId } });
+  }
+  navigateToUserProfile() {
+    this.router.navigate(['profile', this.auth.userId]); // Navigate to user profile with userId as parameter
+  }
+  navigateToFollowUsers() {
+    // Navigate to the follow users page
+    this.router.navigate(['follow-users']);
   }
   title = 'MealMixer';
 }
